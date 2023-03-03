ca7d823db62bd77676663fe918933d686cbf88a7
Graju04 <123477273+Graju04@users.noreply.github.com>
2023-03-03 18:10:21 +0530

    Add files via upload

---
+++ ATM/ATMmenu.py
@@ -0,0 +1,10 @@
+def Menu():
+    print("="*50)
+    print("\tATM Operations")
+    print("="*50)
+    print("\t1.Deposit")
+    print("\t2.Withdraw")
+    print("\t3.Balance Enquiry")
+    print("\t4.Exit")
+    print("="*50)
+ 
